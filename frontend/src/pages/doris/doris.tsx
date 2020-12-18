7e2195ea88fdeeb231ab5a0530683b62cc58edf4
Zapple <zizibaluba@gmail.com>
2020-12-18 09:54:40 -0800

    add doris meme

--- frontend/src/pages/doris/doris.tsx
+++ frontend/src/pages/doris/doris.tsx
@@ -19,8 +19,8 @@ export default class DorisPage extends React.Component<DorisPageProps, DorisPage
                 </p>
                 <p>
                 You have been, and will be, the best DD Hololive CN housed, and who can forget
-                your <a href="https://www.bilibili.com/video/BV1cA41187LW">gachi koi for Rosalyn</a>?
-                Thanks for your <a href="https://www.bilibili.com/video/BV19f4y1Y7Ni">first and last song</a>,
+                your <a href="https://www.bilibili.com/video/BV1cA41187LW">gachi koi for Rosalyn</a><a href="https://www.bilibili.com/video/BV14e411s7jd">?</a> Thanks
+                for your <a href="https://www.bilibili.com/video/BV19f4y1Y7Ni">first and last song</a>,
                 and we won’t forget our promise to count down these next 10 years... To our next meeting!
                 </p>
             </div>
