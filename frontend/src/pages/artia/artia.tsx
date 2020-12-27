f8cc3af77890d1a3d8e7b75973ab2aba47849f74
Zapple <zizibaluba@gmail.com>
2020-12-26 20:28:37 -0800

    chinese text

--- frontend/src/pages/artia/artia.tsx
+++ frontend/src/pages/artia/artia.tsx
@@ -28,6 +28,21 @@ export default class ArtiaPage extends React.Component<ArtiaPageProps, ArtiaPage
                 <p>
                 Most of all, we’ll miss you. <img src="https://u.cubeupload.com/zapple/Petia.gif" alt="Petia"/>
                 </p>
+                <br/><hr/><br/>
+                <p>
+                一个告别。
+                <br/><br/>
+                vvvvvvvvv
+                </p>
+                <br/><br/><br/><br/><br/><br/>
+                <br/><br/><br/><br/><br/>
+                <p>
+                真的，我们已经开始想念你的无数的poggers回议和memes。 我们这么可能忘记你的<a href="https://twitter.com/Kontaruu/status/1235886034824638465">Pepelaugh</a>，
+                你放出的解码游戏，更是你的超越12小时的耐久直播。。。
+                </p>
+                <p>
+                我们会永远挂念你的。 <img src="https://u.cubeupload.com/zapple/Petia.gif" alt="Petia"/>
+                </p>
             </div>
         );
     }
--- frontend/src/pages/doris/doris.tsx
+++ frontend/src/pages/doris/doris.tsx
@@ -23,6 +23,14 @@ export default class DorisPage extends React.Component<DorisPageProps, DorisPage
                 for your <a href="https://www.bilibili.com/video/BV19f4y1Y7Ni">first and last song</a>,
                 and we won’t forget our promise to count down these next 10 years... To our next meeting!
                 </p>
+                <br/><hr/><br/>
+                <p>
+                朵莉丝，你一直把我们当为朋友，家人，我们看你回到大海也很伤心难过。
+                </p>
+                <p>
+                你永远会是Hololive CN最好的DD，你和<a href="https://www.bilibili.com/video/BV1cA41187LW">罗莎琳的gachi恋</a><a href="https://www.bilibili.com/video/BV14e411s7jd">也是个我们不可忘的记忆</a>。
+                谢谢你的<a href="https://www.bilibili.com/video/BV19f4y1Y7Ni">第一和最后一首歌</a>，我们不会忘记我们和你的十年之约。。。每天我们在期待过10年后再次相逢！
+                </p>
             </div>
         );
     }
